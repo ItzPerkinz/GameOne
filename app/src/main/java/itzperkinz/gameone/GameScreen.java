39a93c9e006c06bca87045841a903998da4fa35e
ItzPerkinz <joey.perkins.ksu@gmail.com>
2018-06-10 01:17:16 -0500

    I added some image files. I have it worked out on paper how the game will actually function, I just need to learn how to get those images on the screen. The game logic I understand.

---
+++ app/src/main/java/itzperkinz/gameone/GameScreen.java
@@ -0,0 +1,16 @@
+package itzperkinz.gameone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+
+    }
+}
--- app/src/main/java/itzperkinz/gameone/HomeScreen.java
+++ app/src/main/java/itzperkinz/gameone/HomeScreen.java
@@ -2,12 +2,28 @@ package itzperkinz.gameone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeScreen extends AppCompatActivity {
+
+    List<String> lb = new ArrayList<String>();
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        lv = (ListView) findViewById(R.id.leaderboard);
+        ArrayAdapter ada = new ArrayAdapter(lv.getContext(), android.R.layout.simple_list_item_1, lb);
+        lv.setAdapter(ada);
+    }
+
+    public void StartGame() {
+
     }
 }
